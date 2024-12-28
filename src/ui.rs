e4cbfb3599cf747f7746176ce5f43c6845db5add
rottedfm <rottedfm@proton.me>
2024-12-27 20:10:04 -0500

    ratatui template

---
+++ src/ui.rs
@@ -0,0 +1,11 @@
+use ratatui::{
+    layout::Alignment,
+    style::{Color, Style},
+    widgets::{Block, BorderType, Paragraph},
+    Frame,
+};
+
+use crate::app::App;
+
+/// Renders the user interface widgets.
+pub fn render(app: &mut App, frame: &mut Frame) {}
